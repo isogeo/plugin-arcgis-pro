d5a07295646a43facd65ed8db12fd120e36d950c
Vianney Doleans <vianney.doleans@epitech.eu>
2019-12-03 15:49:49 +0100

    [Add] Realize search tab's skeleton (XAML)

---
+++ ArcGis-Pro/Arcgis-Pro-Isogeo/UI/Search/SearchManagment/AddSearchManagment.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Arcgis_Pro_Isogeo.UI.Search.SearchManagment
+{
+    /// <summary>
+    /// Logique d'interaction pour AddSearchManagment.xaml
+    /// </summary>
+    public partial class AddSearchManagment : Window
+    {
+        public AddSearchManagment()
+        {
+            InitializeComponent();
+        }
+    }
+}
