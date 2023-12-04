9530676b1a65e08d72d0af10a90b2fd0991b652a
Vianney DOLEANS <vianney.doleans@gmail.com>
2023-12-04 09:00:00 +0100

    chore: regenerate all remaining isogeo projects to new csproj format

--- Language/Resources.Designer.cs
+++ Language/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Isogeo.Language {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Isogeo.Language.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Isogeo.Languages.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
